9892be3d59595508efa97af9f257fcc6f7fbbabb
Praveen Kumar pachouri <88274350+praveenpachouri2001@users.noreply.github.com>
2024-10-14 17:34:55 +0530

    Update updatefrontendnew.sh
    updating minikube ip

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Set the Instance ID and path to the .env file
-INSTANCE_ID="i-030da7d31a1dbbffc"
-
-# Retrieve the public IP address of the specified EC2 instance
-ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+# Retrieve the Minikube IP address
+ipv4_address=$(minikube ip)
 
 # Path to the .env file
 file_to_find="../frontend/.env.docker"
